2bc3e8c282120fe995e7fe18676c8ae58ce04996
Jim <jim_the_miller@hotmail.com>
2010-10-11 21:09:52 +1030

    added plugin for blocking item use, minecarts atm

---
+++ blockitemuse/src/blockitemuse.java
@@ -0,0 +1,28 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class blockitemuse extends Plugin {
+
+
+	static final Logger log = Logger.getLogger("Minecraft");
+
+	
+    public void enable() {
+		log.info("[blockitemplace] Mod Enabled.");
+    }
+
+    public void disable() {
+		log.info("[blockitemplace] Mod Disabled");
+    }
+    
+
+	public boolean onBlockCreate(Player player,Block blockPlaced,  Block blockClicked, int itemInHand) {
+		if( itemInHand==328 || itemInHand==66 ) {
+			player.sendMessage("The use of this item has been blocked because the item could crash clients");
+			return true;
+		} else {
+			return false;	
+		}
+	}
+
+}
