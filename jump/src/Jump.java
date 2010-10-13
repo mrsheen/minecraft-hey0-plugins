407299f357f50a081a5f4a28d68c7cae8ef221e2
mrsheen <info@mrsheenau.com>
2010-10-13 12:51:14 +1000

    Improved name matchin of jump using hey0 functions

--- jump/src/Jump.java
+++ jump/src/Jump.java
@@ -34,32 +34,41 @@ public class Jump extends Plugin {
         	if (split.length < 2) {
           		 player.sendMessage(Colors.Red + split[0] + " <Username>");
           		 return true;
-              }
+			}
           	
-               List<Player> players = getPlayersWithPrefix(split[1]);
-               
-              
-       		if (players.size() < 1) {
-       		   	player.sendMessage(Colors.Red + "No player with that name.");
+			
+			Player destplayer = etc.getServer().matchPlayer(split[1]);
+
+		   
+
+			if (destplayer != null) {
+				 if (destplayer.getName().equalsIgnoreCase(player.getName())) {
+					player.sendMessage(Colors.Rose + "You're already here!");
+					return true;
+				}
+				
+				
+				if (etc.getServer().isTimerExpired(player.getName().toLowerCase() + destplayer.getName().toLowerCase())) {
+					player.sendMessage(Colors.Red + "You can request a jump to a player once every 30 seconds");
+					return true;
+				}
+				else
+				{
+					destplayer.sendMessage(Colors.Red + player.getName() + " wants to jump to your location. Use the command /accept " + player.getName() + " to accept, you have 30 seconds.");
+					player.sendMessage(Colors.Red + "Request sent to " + destplayer.getName() + ".");
+					etc.getServer().setTimer(player.getName().toLowerCase() + destplayer.getName().toLowerCase(), 500);
+					return true;
+				}
+				
+			} else {
+				player.sendMessage(Colors.Rose + "Can't find user " + split[1] + ".");
+				
+       		   	
        		   	return true;
-       		} else if (players.size() > 1) {
-       		 	player.sendMessage(Colors.Red + "More than one player with that name, please try again with the full username.");
-       		  	return true;
-       		}
+			}
+			
        		
-       	   	Player p = players.get(0);
-        	
-        	if (etc.getServer().isTimerExpired(player.getName().toLowerCase() + p.getName().toLowerCase())) {
-        		player.sendMessage(Colors.Red + "You can request a jump to a player once every 30 seconds");
-        		return true;
-        	}
-        	else
-        	{
-        		p.sendMessage(Colors.Red + player.getName() + " wants to jump to your location. Use the command /accept " + player.getName() + " to accept, you have 30 seconds.");
-        		player.sendMessage(Colors.Red + "Request sent to " + p.getName() + ".");
-        		etc.getServer().setTimer(player.getName().toLowerCase() + p.getName().toLowerCase(), 500);
-        		return true;
-        	}
+       		
         
         } else if (split[0].equalsIgnoreCase("/accept")) {
         	
@@ -68,30 +77,39 @@ public class Jump extends Plugin {
           		 return true;
               }
           	
-               List<Player> players = getPlayersWithPrefix(split[1]);
+			Player sourceplayer = etc.getServer().matchPlayer(split[1]);
+
+		   
+
+			if (sourceplayer != null) {
+				 if (sourceplayer.getName().equalsIgnoreCase(player.getName())) {
+					player.sendMessage(Colors.Rose + "You're already here!");
+					return true;
+				}
+				
+				
+				if (! etc.getServer().isTimerExpired(sourceplayer.getName().toLowerCase() + player.getName().toLowerCase()))
+				{
+					player.sendMessage(Colors.Red + "FAILED: The player must request a teleport and you must respond within 30 seconds");
+					return true;
+				}
+				else {
+					sourceplayer.teleportTo(player);
+					sourceplayer.sendMessage(Colors.Red + "Woosh!");
+					etc.getServer().setTimer(sourceplayer.getName().toLowerCase() + player.getName().toLowerCase(), 1);
+					return true;
+				}
+				
+			} else {
+				player.sendMessage(Colors.Rose + "Can't find user " + split[1] + ".");
+				
+       		   	
+       		   	return true;
+			}
+			 
                
               
-       		if (players.size() < 1) {
-       		   	player.sendMessage(Colors.Red + "No player with that name.");
-       		   	return true;
-       		} else if (players.size() > 1) {
-       		 	player.sendMessage(Colors.Red + "More than one player with that name, please try again with the full username.");
-       		  	return true;
-       		}
        		
-       	   	Player p = players.get(0);
-        	
-			if (! etc.getServer().isTimerExpired(p.getName().toLowerCase() + player.getName().toLowerCase()))
-			{
-				player.sendMessage(Colors.Red + "FAILED: The player must request a teleport and you must respond within 30 seconds");
-				return true;
-			}
-			else {
-				p.teleportTo(player);
-				p.sendMessage(Colors.Red + "Woosh!");
-				etc.getServer().setTimer(p.getName().toLowerCase() + player.getName().toLowerCase(), 1);
-				return true;
-			}
             
         }
         else {
