9393bc2a13a8208b26d3dc90e93d2a62ad7f7587
Jim <jim_the_miller@hotmail.com>
2010-10-20 05:54:22 +1030

    test plugin for teleports

---
+++ Stargate/src/Stargate.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Stargate extends Plugin {
+
+
+    private StargateListener listener = new StargateListener();
+
+	static final Logger log = Logger.getLogger("Minecraft");
+	
+    public void enable() {
+		log.info("[Stargate] Mod Enabled.");
+    }
+
+    public void disable() {
+		log.info("[Stargate] Mod Disabled");
+    }
+    
+    public void initialize() {
+    	etc.getLoader().addListener(PluginLoader.Hook.COMMAND, listener, this, PluginListener.Priority.MEDIUM);
+    	etc.getLoader().addListener(PluginLoader.Hook.BLOCK_CREATED, listener, this, PluginListener.Priority.MEDIUM);
+    	etc.getLoader().addListener(PluginLoader.Hook.PLAYER_MOVE, listener, this, PluginListener.Priority.MEDIUM);
+    }
+
+
+    public class StargateListener extends PluginListener
+    {
+
+	public boolean onBlockCreate(Player player, Block blockPlaced, Block blockClicked, int itemInHand){
+		if ( (itemInHand==285) && player.canUseCommand("/saveStargate")){
+					Integer whichPoint = StargateData.setPoint(player.getName(), blockClicked.getX(), blockClicked.getY(), blockClicked.getZ());
+					if(whichPoint == 2) {
+						player.sendMessage("First corner of teleport area set");
+					} else if (whichPoint == 3) {
+						player.sendMessage("Second corner of teleport area set");
+					} else if (whichPoint == 4) {
+						player.sendMessage("First corner of landing area set");
+					} else {
+						player.sendMessage("Second corner of landing area set"); 
+					}
+					return true;
+			}
+			return false;
+		}
+			
+
+
+    public boolean onCommand(Player player, String[] split) {
+        if (!player.canUseCommand(split[0])) {
+            return false;
+        }
+        if (split[0].equalsIgnoreCase("/saveStargate")) {
+        	if (split.length == 3) {
+	        	StargateData.Savestargate(player,split);
+        	}
+			return true;
+        } else if (split[0].equalsIgnoreCase("/listStargates")) {
+        	return true;
+        } else if (split[0].equalsIgnoreCase("/removeStargate")) {
+        	boolean worked = false;
+        	if (split.length>1) {
+        	}
+        	if( worked ) {
+        		player.sendMessage("Stargate removed");
+        	} else {
+        		player.sendMessage("Failed to remove Stargate");
+        	}
+        	return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public void onPlayerMove(Player player,Location from,Location to) {
+    	StargateData.Checkplayerpos(to,player);
+    	return;
+    }
+    }
+
+}
---
+++ Stargate/src/StargateData.java
@@ -0,0 +1,176 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class StargateData {
+	
+	static Server world = etc.getServer();
+	static final Logger log = Logger.getLogger("Minecraft");
+	
+	static ArrayList<String> StargateList = new ArrayList<String>();
+	static ArrayList<Integer> StargateLocations = new ArrayList<Integer>();
+	
+	static ArrayList<String> playerList = new ArrayList<String>();
+	static ArrayList<Integer> selectionStatus = new ArrayList<Integer>();
+	static ArrayList<Integer> pointsCoordinates = new ArrayList<Integer>();
+		
+	private static int getPlayerIndex(String playerName){
+		
+		boolean inList = false;
+		for (String p : playerList){
+			if (p==playerName)
+				inList = true;
+		}
+		
+		if (!inList){
+			playerList.add(playerName);
+			selectionStatus.add(1);
+			pointsCoordinates.add(null);	
+			pointsCoordinates.add(null);
+			pointsCoordinates.add(null);
+			pointsCoordinates.add(null);
+			pointsCoordinates.add(null);
+			pointsCoordinates.add(null);
+			pointsCoordinates.add(null);
+			pointsCoordinates.add(null);
+			pointsCoordinates.add(null);
+			pointsCoordinates.add(null);
+			pointsCoordinates.add(null);
+			pointsCoordinates.add(null);
+		}
+				
+		return playerList.indexOf(playerName);
+	}
+	
+	public static int setPoint(String playerName, int X, int Y, int Z){
+		
+		int index = getPlayerIndex(playerName);
+		int secondPoint = selectionStatus.get(index);
+		if ( selectionStatus.get(index) == 1 ){
+			pointsCoordinates.set(index*12, X);
+			pointsCoordinates.set(index*12+1, Y);
+			pointsCoordinates.set(index*12+2, Z);
+			pointsCoordinates.set(index*12+3, null);
+			pointsCoordinates.set(index*12+4, null);
+			pointsCoordinates.set(index*12+5, null);
+			pointsCoordinates.set(index*12+6, null);
+			pointsCoordinates.set(index*12+7, null);
+			pointsCoordinates.set(index*12+8, null);
+			pointsCoordinates.set(index*12+9, null);
+			pointsCoordinates.set(index*12+10, null);
+			pointsCoordinates.set(index*12+11, null);
+			selectionStatus.set(index, 2);
+			
+		}
+		else if ( selectionStatus.get(index) == 2){
+			pointsCoordinates.set(index*12+3, X);
+			pointsCoordinates.set(index*12+4, Y);
+			pointsCoordinates.set(index*12+5, Z);
+			selectionStatus.set(index, 3);
+		}
+		else if ( selectionStatus.get(index) == 3){
+			pointsCoordinates.set(index*12+6, X);
+			pointsCoordinates.set(index*12+7, Y);
+			pointsCoordinates.set(index*12+8, Z);
+			selectionStatus.set(index, 4);
+		}
+		else {
+			pointsCoordinates.set(index*12+9, X);
+			pointsCoordinates.set(index*12+10, Y);
+			pointsCoordinates.set(index*12+11, Z);
+			selectionStatus.set(index, 1);
+		}
+		return selectionStatus.get(index);
+	}
+	
+	public static void Savestargate(Player player, String[] split) {
+		int playerindex = getPlayerIndex(player.getName())*12;
+		if(pointsCoordinates.get(playerindex+11) != null) {
+			for(int i=0;i<3;i++) {
+				if(pointsCoordinates.get(playerindex+i) > pointsCoordinates.get(playerindex+i+3)) {
+					StargateLocations.add(pointsCoordinates.get(playerindex+i+3));
+					StargateLocations.add(pointsCoordinates.get(playerindex+i)+1);
+				} else {
+					StargateLocations.add(pointsCoordinates.get(playerindex+i));
+					StargateLocations.add(pointsCoordinates.get(playerindex+i+3)+1);
+				}
+			}
+			for(int i=6;i<9;i++) {
+				if(pointsCoordinates.get(playerindex+i) > pointsCoordinates.get(playerindex+i+3)) {
+					StargateLocations.add(pointsCoordinates.get(playerindex+i+3));
+					StargateLocations.add(pointsCoordinates.get(playerindex+i)+1);
+				} else {
+					StargateLocations.add(pointsCoordinates.get(playerindex+i));
+					StargateLocations.add(pointsCoordinates.get(playerindex+i+3)+1);
+				}
+			}
+			player.sendMessage("Stargate saved");
+		} else {
+			player.sendMessage("Incorrect");
+		}
+		return;
+	}
+	
+	public static void Checkplayerpos(Location to,Player player) {
+		if(StargateLocations.size()>0) {
+			for(int i=0;i<StargateLocations.size()/12;i++) {
+				if(player.getX()>=StargateLocations.get(i*12) && player.getX()<StargateLocations.get(i*12+1) &&
+				player.getY()>=StargateLocations.get(i*12+2) && player.getY()<StargateLocations.get(i*12+3) &&
+				player.getZ()>=StargateLocations.get(i*12+4) && player.getZ()<StargateLocations.get(i*12+5) ) {
+					player.sendMessage("woosh");
+					
+					player.teleportTo(fromto((double)StargateLocations.get(i*12), 
+											(double)StargateLocations.get(i*12+1), 
+											(double)StargateLocations.get(i*12+6), 
+											(double)StargateLocations.get(i*12+7), 
+											player.getX()),
+					fromto((double)StargateLocations.get(i*12+2), 
+											(double)StargateLocations.get(i*12+3), 
+											(double)StargateLocations.get(i*12+8), 
+											(double)StargateLocations.get(i*12+9), 
+											player.getY()),
+					fromto((double)StargateLocations.get(i*12+4), 
+											(double)StargateLocations.get(i*12+5), 
+											(double)StargateLocations.get(i*12+10), 
+											(double)StargateLocations.get(i*12+11), 
+											player.getZ()),
+					player.getRotation(),
+					player.getPitch());
+				}
+			}
+		}
+	}
+	
+	public static double fromto(double minfrom, double maxfrom, double minto, double maxto, double playerloc) {
+	
+	return (minto + ((maxto-minto) * (playerloc - minfrom) / (maxfrom - minfrom)));	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
