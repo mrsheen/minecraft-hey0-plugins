25ee7827c6171910b7b79b8f2690576dc7a7b3a2
mrsheen <info@mrsheenau.com>
2010-11-02 01:14:51 +1000

    Working skeleton

--- test/src/Test.java
+++ test/src/Test.java
@@ -19,6 +19,9 @@ public class Test extends Plugin {
 	
 	PluginRegisteredListener blockdestroyListener;
 	blockdestroyListener listener_blockdestroy = new blockdestroyListener();
+    
+    PluginRegisteredListener blockcreateListener;
+	blockcreateListener listener_blockcreate = new blockcreateListener();
 	
     public void enable() {
 		log.info("[Portal Test] Mod Enabled.");
@@ -31,17 +34,20 @@ public class Test extends Plugin {
 		etc.getLoader().removeListener(onportalcreateListener);
 		etc.getLoader().removeListener(onportaldestroyListener);
 		etc.getLoader().removeListener(blockdestroyListener);
+        etc.getLoader().removeListener(blockcreateListener);
     }
     
     public void initialize() {
-    	onportalentranceListener = etc.getLoader().addListener(PluginLoader.Hook.PORTAL_ENTRANCE, listener_portenter, this, PluginListener.Priority.MEDIUM);
-    	onportalcreateListener = etc.getLoader().addListener(PluginLoader.Hook.PORTAL_CREATED, listener_portcreate, this, PluginListener.Priority.MEDIUM);
-    	onportaldestroyListener = etc.getLoader().addListener(PluginLoader.Hook.PORTAL_DESTROYED, listener_portdestroy, this, PluginListener.Priority.MEDIUM);
+    	onportalentranceListener = etc.getLoader().addListener(PluginLoader.Hook.PORTALWARP, listener_portenter, this, PluginListener.Priority.MEDIUM);
+    	onportalcreateListener = etc.getLoader().addListener(PluginLoader.Hook.PORTALCREATE, listener_portcreate, this, PluginListener.Priority.MEDIUM);
+    	onportaldestroyListener = etc.getLoader().addListener(PluginLoader.Hook.PORTALDESTROY, listener_portdestroy, this, PluginListener.Priority.MEDIUM);
     	blockdestroyListener = etc.getLoader().addListener(PluginLoader.Hook.BLOCK_DESTROYED, listener_blockdestroy, this, PluginListener.Priority.MEDIUM);
+        blockcreateListener = etc.getLoader().addListener(PluginLoader.Hook.BLOCK_CREATED,listener_blockcreate, this, PluginListener.Priority.MEDIUM);
     }
     
     public class onportalentranceListener extends PluginListener {
-		public boolean onPortalEntrance(Player playero, Portal portalFrom) {
+		public boolean onPortalWarp(Player player, Portal portal) {
+            player.sendMessage("Warping from portal: "+portal.Name);
 			//check that there is only one non-blank sign attached to the side of portalFrom
 			//check that the portalTo name is valid
 			//get portalTo
@@ -52,56 +58,68 @@ public class Test extends Plugin {
     }
     
     public class onportalcreateListener extends PluginListener {
-		public boolean onPortalCreate(Player player, Portal portal) {
-			if(single sign attached to top) {
-				if(!multiple rows of text) {
-					if(!text contains delimiter) {
-						//add portal name to list
-						//add portal naming sign to list of blocks to protect
-						//write data to the storage file
-						return false;
-					} else {
-						player.sendMessage("Portal names must not contain the character ;");
-						return true;
-					}
-				} else {
-					player.sendMessage("Naming sign must only have one non-blank line");
-					return true;
-				}
-			} else {
-				player.sendMessage("Naming sign incorrectly positioned. Please put it on the top row of obisidian");
-				return true;
-			}
+		public boolean onPortalCreate(Portal portal) {
+			// if(single sign attached to top) {
+				// if(!multiple rows of text) {
+					// if(!text contains delimiter) {
+						// add portal name to list
+						// add portal naming sign to list of blocks to protect
+						// write data to the storage file
+						// return false;
+					// } else {
+						// player.sendMessage("Portal names must not contain the character ;");
+						// return true;
+					// }
+				// } else {
+					// player.sendMessage("Naming sign must only have one non-blank line");
+					// return true;
+				// }
+			// } else {
+				// player.sendMessage("Naming sign incorrectly positioned. Please put it on the top row of obisidian");
+				// return true;
+			// }
+            return false;
 		}
     }
     
     public class onportaldestroyListener extends PluginListener {
-		public boolean onPortalDestroy(Player player, Portal portal) {
-			if(protected sign blockID == sign) {
-				protect sign blockID =0;
-			} else {
-				log.info("Someone changed the naming sign for portal " + portal.getPortalName());
-				protect sign blockID =0;
-			}
+		public boolean onPortalDestroy(Portal portal) {
+			// if(protected sign blockID == sign) {
+				// protect sign blockID =0;
+			// } else {
+				// log.info("Someone changed the naming sign for portal " + portal.getPortalName());
+				// protect sign blockID =0;
+			// }
 			//remove portal name from list
 			//remove sign block from list of blocks to protect
 			//delete data from file
-			log.info(player.getName() + " destroyed portal " + portal.getPortalName());
+			log.info("Destroyed portal: " + portal.Name);
 			return false;
 		}
     }
     
     public class blockdestroyListener extends PluginListener {
     	public boolean onBlockDestroy(Player player, Block block) {
-    		if(block.getType() == 68 || block.getType() == 63) {
-    			for(int i=0;i<protectedsigns.size()/3;i++) {
-    				if(block.getX() == protectedsigns.get(i*3) ||
-    				block.getY() == protectedsigns.get(i*3+1) ||
-    				block.getZ() == protectedsigns.get(i*3+2)) {
-    					return true;
-    				}
-    			}
-    		}
+    		// if(block.getType() == 68 || block.getType() == 63) {
+    			// for(int i=0;i<protectedsigns.size()/3;i++) {
+    				// if(block.getX() == protectedsigns.get(i*3) ||
+    				// block.getY() == protectedsigns.get(i*3+1) ||
+    				// block.getZ() == protectedsigns.get(i*3+2)) {
+    					// return true;
+    				// }
+    			// }
+    		// }
+    		return false;
+    	}
+    }
+    
+    public class blockcreateListener extends PluginListener {
+    	public boolean onBlockCreate(Player player, Block blockPlaced, Block blockClicked, int itemInHand) {
+    		 if(itemInHand == 259) {
+                // This player may have just created a portal, add to list
+                // to check for subsequent onPortalCreate calls
+                
+    		 }
     		return false;
     	}
     }
