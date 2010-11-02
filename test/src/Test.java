ed2cf83db4b5c85c6aedef2f8ed99d5163188475
Jim <jim_the_miller@hotmail.com>
2010-11-02 19:55:50 +1030

    Adding sign locking

--- test/src/Test.java
+++ test/src/Test.java
@@ -142,7 +142,7 @@ public class Test extends Plugin {
 			if(destsign != null) {
 				for(int i=0;i<4;i++) {
 					if(destsign.getText(i).length()!=0) {
-						if(destsign.getText(i).toLowerCase()=="lock") {
+						if(destsign.getText(i).toLowerCase()=="Lock".toLowerCase()) {
 							signlock = 1;
 						} else if(destportalname=="") {
 							destportalname = destsign.getText(i).toLowerCase();
